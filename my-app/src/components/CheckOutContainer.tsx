1ce34c25d19e02f7dd54b84979238e5a0da2451b
Shijil <shijilcg@gmail.com>
2024-11-04 19:58:32 +0530

    last update

--- my-app/src/components/CheckOutContainer.tsx
+++ my-app/src/components/CheckOutContainer.tsx
@@ -1,8 +1,6 @@
-
-
 export default function CheckOutContainer() {
   return (
-    <div className="flex fixed top-[50%] left-[50%]  translate-x-[-50%] translate-y-[-50%]  bg-[#777575] z-[10000] w-[500px] h-[200px] items-center justify-center text-[20px] text-white font-bold">
+    <div className="flex fixed top-[50%] left-[50%]  translate-x-[-50%] translate-y-[-50%]  bg-[#777575] z-[10000] w-[500px] h-[200px] items-center justify-center text-[20px] text-white font-bold checkoutContainer">
       Thank you for your shopping
     </div>
   );
